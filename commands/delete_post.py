80c5022f3d3462bf7160687d1103e8b89c292e29
kbeautyg <aiservisy@yandex.ru>
2025-05-29 15:59:11 +0300

    Update delete_post.py

--- commands/delete_post.py
+++ commands/delete_post.py
@@ -1,4 +1,3 @@
-# commands/delete_post.py
 from aiogram import Router
 from aiogram.types import Message
 from aiogram.filters import Command
@@ -24,11 +23,12 @@ async def cmd_delete(message: Message):
         await message.answer(TEXTS[lang]['delete_invalid_id'])
         return
     post = supabase_db.db.get_post(post_id)
-    if not post or post.get("user_id") != user_id:
+    if not post or not supabase_db.db.is_user_in_project(user_id, post.get("project_id", -1)):
         await message.answer(TEXTS[lang]['delete_not_found'])
         return
     if post.get("published"):
         await message.answer(TEXTS[lang]['delete_already_published'])
         return
+    # Delete the post
     supabase_db.db.delete_post(post_id)
     await message.answer(TEXTS[lang]['delete_success'].format(id=post_id))
